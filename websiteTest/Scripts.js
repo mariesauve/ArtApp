a5ef8fbc89dfd3292e960bdab6abbba20b389762
Mary <149751752+mariesauve@users.noreply.github.com>
2024-09-20 20:54:30 -0400

    updated

---
+++ websiteTest/Scripts.js
@@ -0,0 +1,9 @@
+﻿const myBox = document.getElementById("myBox");
+
+function changeColor(event) {
+    /*console.log(event);*/
+    event.target.style.backgroundColor = "purple"
+    event.target.textContent = "welcome! ";
+}
+
+myBox.addEventListener("click", changeColor);
